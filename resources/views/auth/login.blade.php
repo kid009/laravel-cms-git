aecb31f9cf7350087d3ebc45e379b02b6061f826
kid009 <pongpoom1985@gmail.com>
2026-03-06 16:43:59 +0700

    change font

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,6 +13,12 @@
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
     <style>
         html,
         body {
@@ -25,6 +31,7 @@
             padding-top: 40px;
             padding-bottom: 40px;
             background-color: #f5f5f5;
+            font-family: "Prompt", sans-serif;
         }
 
         .form-signin {
